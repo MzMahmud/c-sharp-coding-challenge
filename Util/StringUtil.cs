c9eda6a8b76d6577d2a9b5cd6945f49c292d5bbf
MzMahmud <moaz.mahmud.64@gmail.com>
2024-12-18 18:56:16 +0600

    handled digit chars

--- Util/StringUtil.cs
+++ Util/StringUtil.cs
@@ -1,9 +1,30 @@
-﻿namespace Util;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Util;
 
 public class StringUtil
 {
+    private static readonly string[] KEY_MAP = { " ", "&'(", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+
     public static string OldPhonePad(string input)
     {
-        throw new NotImplementedException("Not implemented.");
+        var output = new List<char>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char currentChar = input[i];
+            if (Char.IsDigit(currentChar))
+            {
+                int keyIndex = currentChar - '0';
+                int charCount = 0;
+                while (i < input.Length && input[i] == currentChar)
+                {
+                    charCount++;
+                    i++;
+                }
+                i--;
+                output.Add(KEY_MAP[keyIndex][charCount - 1]);
+            }
+        }
+        return String.Join("", output);
     }
 }
