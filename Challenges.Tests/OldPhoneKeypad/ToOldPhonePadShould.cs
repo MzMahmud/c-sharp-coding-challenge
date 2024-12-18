b0b9f9fc837f36fb6628b918f5d2231e00e612cb
MzMahmud <moaz.mahmud.64@gmail.com>
2024-12-18 23:43:03 +0600

    updated test cases

--- Challenges.Tests/OldPhoneKeypad/ToOldPhonePadShould.cs
+++ Challenges.Tests/OldPhoneKeypad/ToOldPhonePadShould.cs
@@ -1,3 +1,5 @@
+using Xunit;
+
 namespace Challenges.Tests.OldPhonePad;
 
 public class ToOldPhonePadShould
@@ -6,13 +8,51 @@ public class ToOldPhonePadShould
     [InlineData("", "")]
     [InlineData("CAB", "222 2 22")]
     [InlineData("HELLO", "4433555 555666")]
+    public void Handle_ValuesWithNoSpace(string input, string expected)
+    {
+        var output = OldPhoneKeypad.ToOldPhonePad(input);
+        Assert.Equal(expected, output);
+    }
+
+    [Theory]
+    [InlineData("&", "1")]
+    [InlineData("'", "11")]
+    [InlineData("(", "111")]
+    [InlineData("'&(", "11 1 111")]
+    public void Handle_ValuesWithPunctuation(string input, string expected)
+    {
+        var output = OldPhoneKeypad.ToOldPhonePad(input);
+        Assert.Equal(expected, output);
+    }
+
+    [Theory]
+    [InlineData(" ", "0")]
+    [InlineData("  ", "0 0")]
+    [InlineData("A B  C", "20220 0222")]
     [InlineData(
         "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",
         "844330778844422255022777666966033366699058867 7777066688833777084433055529999 999036664"
     )]
-    public void PassExamples(string input, string expected)
+    public void Handle_ValuesWithSpace(string input, string expected)
     {
         var output = OldPhoneKeypad.ToOldPhonePad(input);
         Assert.Equal(expected, output);
+    }
+
+    public static IEnumerable<object[]> GetInvalidChar()
+    {
+        var invalidChars = "0123456789!@#$%^*){}[].<>/?\"";
+        foreach (var invalidChar in invalidChars)
+        {
+            yield return new object[] { $"{invalidChar}" };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(GetInvalidChar))]
+    public void Handle_ValuesWithInvalidChar(string input)
+    {
+        var exception = Assert.Throws<ArgumentException>(() => OldPhoneKeypad.ToOldPhonePad(input));
+        Assert.Equal($"Contains invalid character '{input}'", exception.Message);
     }
 }
