298edd2a3cba718e1bb5623d3f2bf5d599d41757
MzMahmud <moaz.mahmud.64@gmail.com>
2024-12-23 12:03:15 +0600

    removed IIFE to static constructor, used TryGetValue

--- Challenges/OldPhoneKeypad/OldPhoneKeypad.cs
+++ Challenges/OldPhoneKeypad/OldPhoneKeypad.cs
@@ -5,6 +5,20 @@ namespace Challenges.OldPhoneKeypad;
 public class OldPhoneKeypad
 {
     private static readonly string[] KEY_MAP = [" ", "&'(", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"];
+    private static readonly Dictionary<char, KeyCount> CHAR_KEY_COUNT = [];
+
+    static OldPhoneKeypad()
+    {
+        for (int keyIndex = 0; keyIndex < KEY_MAP.Length; keyIndex++)
+        {
+            for (int charIndex = 0; charIndex < KEY_MAP[keyIndex].Length; charIndex++)
+            {
+                char character = KEY_MAP[keyIndex][charIndex];
+                var keyCount = new KeyCount { Key = (char)(keyIndex + '0'), Count = charIndex + 1 };
+                CHAR_KEY_COUNT.Add(character, keyCount);
+            }
+        }
+    }
 
     public static string OldPhonePad(string input)
     {
@@ -40,24 +54,6 @@ public class OldPhoneKeypad
         return output.ToString();
     }
 
-
-    public static readonly Dictionary<char, KeyCount> CHAR_KEY_COUNT = new Func<Dictionary<char, KeyCount>>(
-        () =>
-        {
-            var map = new Dictionary<char, KeyCount>();
-            for (int keyIndex = 0; keyIndex < KEY_MAP.Length; keyIndex++)
-            {
-                for (int charIndex = 0; charIndex < KEY_MAP[keyIndex].Length; charIndex++)
-                {
-                    char character = KEY_MAP[keyIndex][charIndex];
-                    var keyCount = new KeyCount { Key = (char)(keyIndex + '0'), Count = charIndex + 1 };
-                    map.Add(character, keyCount);
-                }
-            }
-            return map;
-        }
-    )();
-
     public static string ToOldPhonePad(string input)
     {
         var output = new StringBuilder();
@@ -65,17 +61,19 @@ public class OldPhoneKeypad
         foreach (char ch in input)
         {
             char currentChar = char.ToUpper(ch);
-            if (!CHAR_KEY_COUNT.ContainsKey(currentChar))
+            if (CHAR_KEY_COUNT.TryGetValue(currentChar, out var keyCount))
+            {
+                if (lastKey == keyCount.Key)
+                {
+                    output.Append(' ');
+                }
+                output.Append(keyCount.Key, keyCount.Count);
+                lastKey = keyCount.Key;
+            }
+            else
             {
                 throw new ArgumentException($"Contains invalid character '{currentChar}'");
             }
-            var keyCount = CHAR_KEY_COUNT[currentChar];
-            if (lastKey == keyCount.Key)
-            {
-                output.Append(' ');
-            }
-            output.Append(keyCount.Key, keyCount.Count);
-            lastKey = keyCount.Key;
         }
         return output.ToString();
     }
